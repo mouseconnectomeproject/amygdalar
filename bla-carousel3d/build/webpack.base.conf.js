72515099476e01a5d4d59da6bc07e29354b022ea
Amanda <amandatug67@gmail.com>
2020-04-30 12:55:07 -0700

    remove wiring diagram as entry in webpack config

--- bla-carousel3d/build/webpack.base.conf.js
+++ bla-carousel3d/build/webpack.base.conf.js
@@ -13,8 +13,7 @@ function resolve (dir) {
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    home: './src/pages/Home/main.js',
-    wiringDiagram: './src/pages/WiringDiagram/main.js'
+    home: './src/pages/Home/main.js'
   },
   output: {
     path: config.build.assetsRoot,
