f80d139ca89af7f5e5cb0fb888377aff320ba5a4
Amanda <amandatug67@gmail.com>
2020-05-01 11:18:31 -0700

    add header to index

---
+++ bla-carousel3d/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+      <a href="https://cic.ini.usc.edu/">
+        <img src="static/CIC_logo.png"/>
+      </a>
+      <a id="cite" href="https://github.com/mouseconnectomeproject/amygdalar/blob/master/README.md">How To Cite</a>
+    </header>
+</template>
+
+<style scoped>
+image {
+    width: 20%;
+}
+
+#cite {
+  font-family: Garamond;
+  font-size: 15px;
+  position: absolute;
+  right: 10px;
+  top: 10px;
+}
+
+</style>
