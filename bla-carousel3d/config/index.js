594b41bed23192fa8028526dc61f3230b0dba469
Amanda <amandatug67@gmail.com>
2020-05-11 10:18:02 -0700

    maintain development paths for js and css

--- bla-carousel3d/config/index.js
+++ bla-carousel3d/config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist/development'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/amygdalar/',
+    assetsPublicPath: '/amygdalar/development/',
 
     /**
      * Source Maps
